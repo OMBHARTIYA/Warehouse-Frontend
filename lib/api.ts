6b10cbc174808f2867a43adc3f9ad887ebbe1ccc
OM BHARTIYA <117409220+OMBHARTIYA@users.noreply.github.com>
2026-05-16 17:16:53 +0200

    task by status layout change

--- lib/api.ts
+++ lib/api.ts
@@ -6,6 +6,7 @@ const api = axios.create({
     process.env.NEXT_PUBLIC_API_URL ??
     process.env.NEXT_PUBLIC_API_BASE_URL ??
     "http://localhost:3000",
+  timeout: 10000,
 });
 
 api.interceptors.request.use((config) => {
